2e86e64eb463b44b1de4f1177090504c4d90e8c5
APixelVisuals <apixelvisualsofficial@gmail.com>
2019-11-11 07:30:43 -0800

    Added ozzolites (currency) to the users db schema

--- models/users.js
+++ models/users.js
@@ -1,6 +1,10 @@
 module.exports = {
     //User ID
     _id: String,
+    ozzolites: {
+        type: Number,
+        default: 0
+    },
     stats: [{
         name: String,
         count: Number
