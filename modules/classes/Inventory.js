4a0d011be474fc3a8864f29193b759b208a25cac
APixelVisuals <apixelvisualsofficial@gmail.com>
2019-11-11 13:20:50 -0800

    Added methods to manipulate and view items in `Inventory` class

--- modules/classes/Inventory.js
+++ modules/classes/Inventory.js
@@ -4,6 +4,46 @@ module.exports = class Inventory {
         this.items = inv;
     }
 
+    hasItems(name, amount) {
+
+        //No amount
+        if (!amount) amount = 1;
+
+        //Get item
+        const item = this.items.find(i => i.name === name);
+        if (!item) return false;
+
+        if (item.amount < amount) return false;
+
+        return true;
+    }
+
+    addItems(name, amount) {
+
+        //No amount
+        if (!amount) return;
+
+        //Add items
+        if (!this.items.find(i => i.name === name)) this.items.push({ name, amount: 0 });
+
+        const item = this.items.find(i => i.name === name);
+        item.amount = item.amount + amount;
+    }
+
+    removeItems(name, amount) {
+
+        //No amount
+        if (!amount) return;
+
+        //Remove items
+        const item = this.items.find(i => i.name === name);
+        if (!item) return;
+
+        item.amount = item.amount - amount;
+
+        if (item.amount <= 0) this.items.splice(this.items.indexOf(item), 1);
+    }
+
     getItems() {
         return this.items;
     }
