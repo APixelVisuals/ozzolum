04a5f1f4dbb08d9cfc1435e60cafc44b5ffafd85
APixelVisuals <apixelvisualsofficial@gmail.com>
2019-12-01 23:16:06 -0800

    Added locations data

---
+++ modules/misc/locations.js
@@ -0,0 +1,89 @@
+/*
+ *  name: The location name
+ *  channel: The channel ID of the location
+ *  itemImageWhiteText: Whether the text color of item images should be white or not
+ *  chop | mine | dig: Data for the chop/mine/dig commands
+ *      loot:
+ *          name: The item name
+ *          frequency: The % chance that the player will get any amount of this item
+ *          min: The base minimum amount of this item that the player can get
+ *          max: The base maximum amount of this item that the player can get
+*/
+
+module.exports = [
+    {
+        name: "Forest",
+        channel: "648803120830480384",
+        itemImageWhiteText: true,
+        chop: {
+            loot: [
+                {
+                    name: "Oak Log",
+                    frequency: 100,
+                    min: 1,
+                    max: 3
+                },
+                {
+                    name: "Hickory Log",
+                    frequency: 20,
+                    min: 1,
+                    max: 3
+                },
+                {
+                    name: "Stick",
+                    frequency: 5,
+                    min: 1,
+                    max: 4
+                }
+            ]
+        },
+        dig: {
+            loot: [
+                {
+                    name: "Dirt",
+                    frequency: 100,
+                    min: 5,
+                    max: 10
+                },
+                {
+                    name: "Clay",
+                    frequency: 20,
+                    min: 3,
+                    max: 5
+                },
+                {
+                    name: "Pebble",
+                    frequency: 5,
+                    min: 5,
+                    max: 15
+                }
+            ]
+        }
+    },
+    {
+        name: "Mines",
+        channel: "648804143196274700",
+        mine: {
+            loot: [
+                {
+                    name: "Stone",
+                    frequency: 100,
+                    min: 6,
+                    max: 12
+                },
+                {
+                    name: "Pebble",
+                    frequency: 85,
+                    min: 7,
+                    max: 15
+                },
+                {
+                    name: "Coal",
+                    frequency: 10,
+                    min: 3,
+                    max: 7
+                }
+            ]
+        }
+    }
+];
